dd290ea98900daba7940f3b6dfd3d60a92f83bfe
Todd <t.acrossthemountain@uleth.ca>
2026-05-16 02:11:43 -0600

    created drawPoint function to draw a rectangle from a center position given an x and y coordinate

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillRect(0, 400, 10, 10);
+const pointSize = 10;
+
+//draw point rectange filled at center position x, y
+function drawPoint(x, y) {
+    ctx.fillRect(x - pointSize/2, y - pointSize/2, pointSize, pointSize)
+}
+
